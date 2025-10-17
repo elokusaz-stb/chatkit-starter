4e353c5a38302d757665825b95b5411efc0be54f
elokusaz-stb <elokusaz@silvertreebrands.com>
2025-10-17 03:59:44 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 10000,
+    host: "0.0.0.0",
+    allowedHosts: ["chatkit-starter-1.onrender.com"]
+  }
 });
