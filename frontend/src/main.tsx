10f89f10bbc0e4b43f372bfcf287349a9ca2367d
elokusaz-stb <elokusaz@silvertreebrands.com>
2025-10-17 04:44:59 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { MyChat } from "./MyChat";
+import ReactDOM from "react-dom/client";
+import MyChat from "./MyChat";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MyChat />
   </React.StrictMode>
